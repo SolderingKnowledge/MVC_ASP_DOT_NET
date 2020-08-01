cc0673442db1c104406f4e200922f8a001b691cf
SolderingKnowledge <imaikol@yahoo.com>
2020-08-01 15:14:36 -0700

    Add custom rout configs

--- MVC_ASP_DOT_NET/App_Start/RouteConfig.cs
+++ MVC_ASP_DOT_NET/App_Start/RouteConfig.cs
@@ -12,6 +12,13 @@ namespace MVC_ASP_DOT_NET
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            // custom route
+            routes.MapRoute(
+                "MoviesByReleasedate",// name
+                "movies/released/{year}/{month}",//url
+                new { controller = "Movies", action = "ByReleaseDate" }, //defaults
+                new { year = @"\d{4}", month = @"\d{2}" } // setting contraints year to be 4 digits & monthg to to be 2 digits
+            );
 
             routes.MapRoute(
                 name: "Default",
--- MVC_ASP_DOT_NET/Controllers/MoviesController.cs
+++ MVC_ASP_DOT_NET/Controllers/MoviesController.cs
@@ -9,11 +9,51 @@ namespace MVC_ASP_DOT_NET.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
+        // GET: Movies/Random => http://localhost:19811/movies/random
+        // Using this way it is good practice to use below for unit test
+        //public ViewResult Random()
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek" };
             return View(movie);
+
+
+            //Alternatevily we can return this way ActionResult:
+
+            //return new ViewResult();
+            //return Content("Hello world");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //will redirect you to homepage and query strings are added http://localhost:19811/?page=1&sortBy=name
+            //return RedirectToAction("Index", "Home", new { page=1, sortBy="name" });
+        }
+        // GET: http://localhost:19811/movies/edit/1 || http://localhost:19811/movies/edit?id=1 => id=1
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        // GET: http://localhost:19811/movies => pageIndex=1&sortBy=Name || http://localhost:19811/movies?pageIndex=2&sortBy=moon => pageIndex=2&sortBy=moon
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        // Details are in RouteConfig.cs
+        // GET: movies/released/{year}/{month}" => http://localhost:19811/movies/released/2015/january => 2015/january
+        //public ActionResult ByReleaseDate(int year, string month) => 2015/January
+        public ActionResult ByReleaseDate(int year, int month)// 2015/4
+        {
+            
+            return Content(year + "/" + month);
         }
     }
 }
